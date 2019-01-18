b9f2e8fa9b0492f0275387a9441a253021790e8d
torchegit <tartaruga9452@gmail.com>
2019-01-18 21:49:12 +0900

    19/01/18

--- WebProj2/src/com/internousdev/webproj2/dao/LoginDAO.java
+++ WebProj2/src/com/internousdev/webproj2/dao/LoginDAO.java
@@ -1,6 +1,9 @@
 package com.internousdev.webproj2.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.internousdev.webproj2.util.DBConnector;
 
@@ -14,5 +17,27 @@ public class LoginDAO {
 		boolean ret=false;
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
+
+
+		String sql = "select * from users where user_name=? and password=?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				this.username=rs.getString("user_name");
+				this.password=rs.getString("password");
+				ret=true;
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			con.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ret;
 	}
 }
