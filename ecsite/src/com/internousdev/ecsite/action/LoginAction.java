61cb370075c2454736d7eaadfc72c6dda68e6da5
naconeco <antubu12@gmail.com>
2019-03-22 21:37:28 +0900

    20190322

--- ecsite/src/com/internousdev/ecsite/action/LoginAction.java
+++ ecsite/src/com/internousdev/ecsite/action/LoginAction.java
@@ -27,7 +27,7 @@ public class LoginAction extends ActionSupport implements SessionAware {
 						BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
 
 						session.put("login_user_id", loginDTO.getLoginId());
-						session.put("Id", buyItemDTO.getId());
+						session.put("id", buyItemDTO.getId());
 						session.put("buyItem_name", buyItemDTO.getItemName());
 						session.put("buyItem_price", buyItemDTO.getItemPrice());
 
